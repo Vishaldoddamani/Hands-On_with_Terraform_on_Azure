f73520453e95e80a387ff1db3921300b2a3243f5
vishal D <vishal.doddamani@gmail.com>
2025-07-05 20:04:20 +0100

    corrected

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ provider "azurerm" {
   features {
 
   }
-  subscription_id = "28e1e42a-4438-4c30-9a5f-7d7b488fd883"
+  subscription_id                 = "28e1e42a-4438-4c30-9a5f-7d7b488fd883"
   resource_provider_registrations = "none"
 }
 
